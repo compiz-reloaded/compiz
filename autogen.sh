1c5fbaf04ec363919cf9bf471bbce2fd8f8d2af6
Jeremy C. Reed <reed@reedmedia.net>
2006-03-18 06:32:45 +0000

    Use same autogen.sh as rest of modular Xorg. This allows running it from     alternative work directory.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#! /bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+
 autoreconf -v --install || exit 1
-./configure "$@"
+cd $ORIGDIR || exit $?
+
+$srcdir/configure --enable-maintainer-mode "$@"
+
