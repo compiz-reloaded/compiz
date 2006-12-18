ec2e927271b4674993157bb35bb9c8543afeb40e
David Reveman <davidr@novell.com>
2006-12-19 00:21:21 +0100

    Fix license info. LGPL header was a cut-n-paste mistake. All kde window decorator code is GPL2.

--- kde/window-decorator/decorator.cpp
+++ kde/window-decorator/decorator.cpp
@@ -3,20 +3,20 @@
  * Copyright © 2006 Dennis Kasprzyk <onestone@beryl-project.org>
  * Copyright © 2006 Volker Krause <vkrause@kde.org>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Lesser General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
  */
 
 #include <qglobal.h>
@@ -946,4 +946,3 @@ KWD::Decorator::sendClientMessage (WId  eventWid,
     XSendEvent (qt_xdisplay (), eventWid, false, mask, &ev);
     KWD::popXError ();
 }
-
