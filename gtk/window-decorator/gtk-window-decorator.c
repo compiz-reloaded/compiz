8356210ed137add8c62a56330680a09b11b6766a
David Reveman <davidr@novell.com>
2007-08-02 17:09:19 -0400

    Use gtk_style_attach properly in update_style.

--- gtk/window-decorator/gtk-window-decorator.c
+++ gtk/window-decorator/gtk-window-decorator.c
@@ -5422,11 +5422,15 @@ update_style (GtkWidget *widget)
     decor_color_t spot_color;
 
     style = gtk_widget_get_style (widget);
-    gtk_style_attach (style, widget->window);
+    g_object_ref (G_OBJECT (style));
+
+    style = gtk_style_attach (style, widget->window);
 
     spot_color.r = style->bg[GTK_STATE_SELECTED].red   / 65535.0;
     spot_color.g = style->bg[GTK_STATE_SELECTED].green / 65535.0;
     spot_color.b = style->bg[GTK_STATE_SELECTED].blue  / 65535.0;
+
+    g_object_unref (G_OBJECT (style));
 
     shade (&spot_color, &_title_color[0], 1.05);
     shade (&_title_color[0], &_title_color[1], 0.85);
