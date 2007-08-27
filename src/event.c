a3f23a4307aeaac836e79d95212272d40614b122
Danny Baumann <dannybaumann@web.de>
2007-08-27 14:33:39 +0200

    Never modify w->state outside changeWindowState function.

--- src/event.c
+++ src/event.c
@@ -1312,9 +1312,9 @@ handleEvent (CompDisplay *d,
 		if (w->state & CompWindowStateHiddenMask)
 		{
 		    w->minimized = FALSE;
-		    w->state &= ~CompWindowStateHiddenMask;
 
-		    changeWindowState (w, w->state);
+		    changeWindowState (w,
+				       w->state & ~CompWindowStateHiddenMask);
 
 		    updateClientListForScreen (w->screen);
 		}
@@ -1682,12 +1682,10 @@ handleEvent (CompDisplay *d,
 				  CompWindowStateMaximizedVertMask))
 			stackingUpdateMode = CompStackingUpdateModeNormal;
 
-		    w->state = wState;
+		    changeWindowState (w, wState);
 
 		    recalcWindowType (w);
 		    recalcWindowActions (w);
-
-		    changeWindowState (w, w->state);
 
 		    updateWindowAttributes (w, stackingUpdateMode);
 		}
