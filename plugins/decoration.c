97b0677fece370389db02d493678542affbd5243
Danny Baumann <dannybaumann@web.de>
2007-09-14 08:57:07 +0200

    Revert "Handle MapRequest event after core so that decorWindowUpdate sees window type and override_redirect state changes that might have happened during event processing."
    This reverts commit 8af9c24dee10bf827f1784db1590d0d9bcae9a93.
    
    The decoration must be added before core handles the map request so that it can be taken into consideration properly for the initial window placement.

--- plugins/decoration.c
+++ plugins/decoration.c
@@ -945,6 +945,11 @@ decorHandleEvent (CompDisplay *d,
 		decorCheckForDmOnScreen (w->screen, TRUE);
 	}
 	break;
+    case MapRequest:
+	w = findWindowAtDisplay (d, event->xmaprequest.window);
+	if (w)
+	    decorWindowUpdate (w, TRUE);
+	break;
     default:
 	if (event->type == d->damageEvent + XDamageNotify)
 	{
@@ -999,11 +1004,6 @@ decorHandleEvent (CompDisplay *d,
     }
 
     switch (event->type) {
-    case MapRequest:
-	w = findWindowAtDisplay (d, event->xmaprequest.window);
-	if (w)
-	    decorWindowUpdate (w, TRUE);
-	break;
     case PropertyNotify:
 	if (event->xproperty.atom == dd->winDecorAtom)
 	{
