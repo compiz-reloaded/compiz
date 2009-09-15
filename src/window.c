d8285f4f8c6ca50b69965a225c36cdad1e456d24
Erkin Bahceci <erkinbah@gmail.com>
2009-09-15 00:31:42 -0500

    Update client list after unhook/insert.

--- src/window.c
+++ src/window.c
@@ -4214,6 +4214,8 @@ lowerWindow (CompWindow *w)
 	focusedWindow = focusTopmostWindow (w->screen);
 	insertWindowIntoScreen (w->screen, w, aboveId);
 
+	updateClientListForScreen (w->screen);
+
 	/* if the newly focused window is a desktop window,
 	   give the focus back to w */
 	if (focusedWindow && focusedWindow->type & CompWindowTypeDesktopMask)
