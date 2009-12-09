bbf3128630f5f6b0f1839e9602c5b417fbb05c32
Danny Baumann <dannybaumann@web.de>
2009-12-09 16:35:48 +0100

    Revert "Don't pull in unstable API, it's not needed any longer."
    This reverts commit 9cb2dffa18f711a8515e65612721900ba5cd22e3.

--- kde/window-decorator-kde4/window.h
+++ kde/window-decorator-kde4/window.h
@@ -50,7 +50,7 @@ namespace KWin
 
 namespace KWD
 {
-class Window: public QObject, public KDecorationBridge {
+class Window: public QObject, public KDecorationBridgeUnstable {
     Q_OBJECT public:
 
 	enum Type
@@ -108,8 +108,10 @@ class Window: public QObject, public KDecorationBridge {
 	virtual QWidget *initialParentWidget (void) const;
 	virtual Qt::WFlags initialWFlags (void) const;
 	virtual void grabXServer (bool grab);
-	virtual bool compositingActive() const;
 
+	/* unstable API */
+	virtual bool compositingActive () const;
+	
 	virtual bool eventFilter (QObject* o, QEvent* e);
 
 	void handleActiveChange (void);
