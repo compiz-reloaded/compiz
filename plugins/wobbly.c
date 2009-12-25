af6a23a1e9e325a2488cfeceb12741bb1ab21e6c
Erkin Bahceci <erkinbah@gmail.com>
2009-12-25 14:14:32 -0600

    wobbly: Constrain throwing at the bottom as well.
    Backport from 7d75379a834afaed0e8621751c1b217966122765.

--- plugins/wobbly.c
+++ plugins/wobbly.c
@@ -1709,25 +1709,44 @@ wobblyPreparePaintScreen (CompScreen *s,
 
 			if (!ww->grabbed && ws->grabWindowWorkArea)
 			{
+			    float topmostYPos    = MAXSHORT;
 			    float bottommostYPos = MINSHORT;
-			    int   i, decorTop;
+			    int   decorTop;
+			    int   decorTitleBottom;
+			    int   i;
 
-			    /* find the bottommost top-row object */
 			    for (i = 0; i < GRID_WIDTH; i++)
 			    {
 				int modelY = model->objects[i].position.y;
+
+				/* find the bottommost top-row object */
 				bottommostYPos = MAX (modelY, bottommostYPos);
+
+				/* find the topmost top-row object */
+				topmostYPos = MIN (modelY, topmostYPos);
 			    }
 
 			    decorTop = bottommostYPos +
 				       w->output.top - w->input.top;
+			    decorTitleBottom = topmostYPos + w->output.top;
 
 			    if (ws->grabWindowWorkArea->y > decorTop)
 			    {
-				/* constrain to work area */
+				/* constrain to work area box top edge */
 				modelMove (model, 0,
 					   ws->grabWindowWorkArea->y -
 					   decorTop);
+				modelCalcBounds (model);
+			    }
+			    else if (ws->grabWindowWorkArea->y +
+			    	     ws->grabWindowWorkArea->height <
+			    	     decorTitleBottom)
+			    {
+				/* constrain to work area box bottom edge */
+				modelMove (model, 0,
+					   ws->grabWindowWorkArea->y +
+					   ws->grabWindowWorkArea->height -
+					   decorTitleBottom);
 				modelCalcBounds (model);
 			    }
 			}
