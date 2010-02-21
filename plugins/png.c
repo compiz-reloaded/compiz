f8988488629bff98521cdaec1fa01691a59d7f23
Lukasz Kies <kiesiu@kiesiu.com>
2010-02-21 12:56:22 +0100

    Fix png plugin to work with libpng >= 1.4.

--- plugins/png.c
+++ plugins/png.c
@@ -102,7 +102,7 @@ readPngData (png_struct	*png,
 
     /* expand gray bit depth if needed */
     if (color_type == PNG_COLOR_TYPE_GRAY && depth < 8)
-	png_set_gray_1_2_4_to_8 (png);
+	png_set_expand_gray_1_2_4_to_8 (png);
 
     /* transform transparency to alpha */
     if (png_get_valid(png, info, PNG_INFO_tRNS))
@@ -167,7 +167,7 @@ readPngFileToImage (FILE *file,
     Bool	  status;
 
     sig_bytes = fread (png_sig, 1, PNG_SIG_SIZE, file);
-    if (png_check_sig (png_sig, sig_bytes) == 0)
+    if (png_sig_cmp (png_sig, 0, sig_bytes) != 0)
 	return FALSE;
 
     png = png_create_read_struct (PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
@@ -218,7 +218,7 @@ readPngBuffer (const unsigned char *buffer,
     Bool		status;
 
     memcpy (png_sig, buffer, PNG_SIG_SIZE);
-    if (png_check_sig (png_sig, PNG_SIG_SIZE) == 0)
+    if (png_sig_cmp (png_sig, 0, PNG_SIG_SIZE) != 0)
 	return FALSE;
 
     png = png_create_read_struct (PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
