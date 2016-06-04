64f61a510fffcbfab21da340f7c2e7eeee499671
Sorokin Alexei <sor.alexei@meowr.ru>
2016-06-04 16:49:02 +0300

    Change loglevel to "fatal" in cases that result in fallbacking

--- src/texture.c
+++ src/texture.c
@@ -247,7 +247,7 @@ bindPixmapToTexture (CompScreen  *screen,
     if ((!getenv ("SKIP_CHECKS") || strcmp (getenv ("SKIP_CHECKS"), "yes") != 0)
 	&& (width > screen->maxTextureSize || height > screen->maxTextureSize))
     {
-	compLogMessage ("core", CompLogLevelWarn,
+	compLogMessage ("core", CompLogLevelFatal,
 			"Exceeded max texture size");
 
 	launchFallbackWM ();
@@ -255,7 +255,7 @@ bindPixmapToTexture (CompScreen  *screen,
 
     if (!config->fbConfig)
     {
-	compLogMessage ("core", CompLogLevelWarn,
+	compLogMessage ("core", CompLogLevelFatal,
 			"No GLXFBConfig for depth %d",
 			depth);
 
@@ -301,7 +301,7 @@ bindPixmapToTexture (CompScreen  *screen,
 					       attribs);
     if (!texture->pixmap)
     {
-	compLogMessage ("core", CompLogLevelWarn,
+	compLogMessage ("core", CompLogLevelFatal,
 			"glXCreatePixmap failed");
 
 	launchFallbackWM ();
@@ -347,7 +347,7 @@ bindPixmapToTexture (CompScreen  *screen,
 	texture->mipmap = FALSE;
 	break;
     default:
-	compLogMessage ("core", CompLogLevelWarn,
+	compLogMessage ("core", CompLogLevelFatal,
 			"pixmap 0x%x can't be bound to texture",
 			(int) pixmap);
 
