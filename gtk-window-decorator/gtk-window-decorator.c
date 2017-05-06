1950b71e4e84637874a4fba0caa9cd86577e437b
Scott Moreau <oreaus@gmail.com>
2017-05-05 23:05:21 -0600

    Revert "gtk-w-d: Fix dbus glib support"
    This reverts commit 36541bc15f53f08604724442663c6f20ee64ba75
    because the change was unnecessary.

--- gtk-window-decorator/gtk-window-decorator.c
+++ gtk-window-decorator/gtk-window-decorator.c
@@ -6945,7 +6945,7 @@ dbus_handle_message (DBusConnection *connection,
 	!strcmp (path[1], "freedesktop") &&
 	!strcmp (path[2], "compiz")      &&
 	!strcmp (path[3], "decoration")  &&
-	!strcmp (path[4], "display"))
+	!strcmp (path[4], "allscreens"))
     {
 	result = DBUS_HANDLER_RESULT_HANDLED;
 
