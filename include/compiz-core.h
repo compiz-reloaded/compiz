8155dd3970c1debca4cdb968d94f8c5dc9ffcd0b
lukefromdc <lukefromdc@hushmail.com>
2018-02-22 23:16:57 -0500

    Window: Add clientFrame(), to get the eventually set CSD extents
    Re-apply 8f8e6ec6017db65b3112e66b78675efec4170c2b
    "Window: Add clientFrame(), to get the eventually set CSD extents"
    
    and 8f8e6ec6017db65b3112e66b78675efec4170c2b
    "Restore ABI compatibility broken by the previous commit"
    
    This is necessary to allow plugins such as "move" and "grid" to accomodate CDS extents and shadow areas

--- include/compiz-core.h
+++ include/compiz-core.h
@@ -2690,6 +2690,7 @@ struct _CompWindow {
     DrawWindowGeometryProc drawWindowGeometry;
 
     /* placed here to preserve ABI compat */
+    CompWindowExtents clientFrame;
     CompWindowExtents frameInput;
 };
 
@@ -2817,6 +2818,9 @@ updateTransientHint (CompWindow *w);
 
 void
 updateIconGeometry (CompWindow *w);
+
+void
+updateClientFrame (CompWindow *w);
 
 Window
 getClientLeader (CompWindow *w);
