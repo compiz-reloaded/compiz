d1017c637b5d3cc56cedef01b3e0968837cb4c76
Colomban Wendling <cwendling@hypra.fr>
2019-12-12 09:45:16 +0100

    place: Avoid round-trip maximizing windows that don't fit the workspace
    Use maximizeWindow() instead of sending an X event to ask for
    maximization.
    
    See https://gitlab.com/compiz/compiz-core/merge_requests/160

--- plugins/place.c
+++ plugins/place.c
@@ -307,29 +307,6 @@ placeSetScreenOption (CompPlugin      *plugin,
     return FALSE;
 }
 
-static void
-placeSendWindowMaximizationRequest (CompWindow *w)
-{
-    XEvent      xev;
-    CompDisplay *d = w->screen->display;
-
-    xev.xclient.type    = ClientMessage;
-    xev.xclient.display = d->display;
-    xev.xclient.format  = 32;
-
-    xev.xclient.message_type = d->winStateAtom;
-    xev.xclient.window	     = w->id;
-
-    xev.xclient.data.l[0] = 1;
-    xev.xclient.data.l[1] = d->winStateMaximizedHorzAtom;
-    xev.xclient.data.l[2] = d->winStateMaximizedVertAtom;
-    xev.xclient.data.l[3] = 0;
-    xev.xclient.data.l[4] = 0;
-
-    XSendEvent (d->display, w->screen->root, FALSE,
-		SubstructureRedirectMask | SubstructureNotifyMask, &xev);
-}
-
 static Bool
 placeGetPointerPosition (CompScreen *s,
 			 int        *x,
@@ -1447,7 +1424,7 @@ placeDoValidateWindowResizeRequest (CompWindow     *w,
 	    (w->mwmDecor & (MwmDecorAll | MwmDecorTitle))   &&
 	    !(w->state & CompWindowStateFullscreenMask))
 	{
-	    placeSendWindowMaximizationRequest (w);
+	    maximizeWindow (w, MAXIMIZE_STATE);
 	}
     }
 
